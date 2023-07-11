48dcae860b58dd3ff6c4702af20077424083b58a
Dryst <touchfn@gmail.com>
2023-07-11 19:57:24 +0700

    Build for node server

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  localhost: 'https://timetrend-demo.herokuapp.com/',
-  apiUrl: 'https://timetrend-demo.herokuapp.com/api/auth/v1/',
-  apiUserUrl: 'https://timetrend-demo.herokuapp.com/api/user/v1/',
-};
